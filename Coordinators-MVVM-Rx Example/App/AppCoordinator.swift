32aac375bcc098b362311eba7b8038fdb263b4b4
Arthur Myronenko <arthur.myronenko@gmail.com>
2017-06-29 15:07:24 +0300

    Setup project structure

---
+++ Coordinators-MVVM-Rx Example/App/AppCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  AppCoordinator.swift
+//  Coordinators-MVVM-Rx Example
+//
+//  Created by Arthur Myronenko on 6/29/17.
+//  Copyright © 2017 Arthur Myronenko. All rights reserved.
+//
+
+import Foundation
---
+++ Coordinators-MVVM-Rx Example/Library/BaseCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  BaseCoordinator.swift
+//  Coordinators-MVVM-Rx Example
+//
+//  Created by Arthur Myronenko on 6/29/17.
+//  Copyright © 2017 Arthur Myronenko. All rights reserved.
+//
+
+import Foundation
